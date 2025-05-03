89416e3b69b3a9b691414e0c69aeb751d2e4b773
Andrzej Rawski <rawski.andrzej@gmail.com>
2025-05-03 21:36:24 +0200

    added Lombok annotations

--- src/main/java/dto/response/CreateTaskCreatorResponseDto.java
+++ src/main/java/dto/response/CreateTaskCreatorResponseDto.java
@@ -2,7 +2,11 @@ package dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateTaskCreatorResponseDto {
 
@@ -10,19 +14,4 @@ public class CreateTaskCreatorResponseDto {
     private String userName;
     private String email;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public String toString() {
-        return "CreateTaskCreatorResponseDto{" +
-                "userName='" + userName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
